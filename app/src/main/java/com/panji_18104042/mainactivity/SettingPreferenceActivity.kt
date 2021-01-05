e968abea3827cdfca516f634f95992e7a69cae15
Panji AM <72533509+PanjiAM@users.noreply.github.com>
2021-01-05 22:28:03 +0700

    Create new  SettingPreferenceActivity

---
+++ app/src/main/java/com/panji_18104042/mainactivity/SettingPreferenceActivity.kt
@@ -0,0 +1,11 @@
+package com.panji_18104042.mainactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_preference)
+    }
+}
