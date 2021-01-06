e1953122bacab12e5f3158fab6d29d7193319844
Panji AM <72533509+PanjiAM@users.noreply.github.com>
2021-01-06 08:41:03 +0700

    update modul step 18

--- app/src/main/java/com/panji_18104042/mainactivity/MainActivity.kt
+++ app/src/main/java/com/panji_18104042/mainactivity/MainActivity.kt
@@ -1,15 +1,60 @@
 package com.panji_18104042.mainactivity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.activity_main.*
 import com.panji_18104042.mainactivity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var settingModel: SettingModel
+    private lateinit var mSettingPreference : SettingPreference
+
+    companion object {
+        private const val REQUEST_CODE = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.act = this
         setContentView(binding.root)
         supportActionBar?.title = getString(R.string.main_title)
+        mSettingPreference = SettingPreference(this)
+        showExistingPreference()
+    }
+
+
+    private fun showExistingPreference() {
+        settingModel = mSettingPreference.getSetting()
+        populateView(settingModel)
+    }
+    private fun populateView(settingModel: SettingModel) {
+        if (settingModel.isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            delegate.applyDayNight()
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            delegate.applyDayNight()
+        }
+        binding.settingModel = settingModel
+    }
+    fun openSetting(){
+        val intent = Intent(this@MainActivity, SettingPreferenceActivity::class.java)
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == SettingPreferenceActivity.RESULT_CODE) {
+                showExistingPreference()
+            }
+        }
     }
 }
