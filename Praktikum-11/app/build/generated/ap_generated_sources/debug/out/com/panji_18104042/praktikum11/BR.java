75e17704e5a1bcad6fdf41daa54554cdabbfce12
Panji AM <72533509+PanjiAM@users.noreply.github.com>
2021-01-23 22:11:01 +0700

    Praktikum 12

---
+++ Praktikum-11/app/build/generated/ap_generated_sources/debug/out/com/panji_18104042/praktikum11/BR.java
@@ -0,0 +1,5 @@
+package com.panji_18104042.praktikum11;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ Praktikum-11/app/build/generated/data_binding_trigger/debug/com/panji_18104042/praktikum11/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.panji_18104042.praktikum11;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
